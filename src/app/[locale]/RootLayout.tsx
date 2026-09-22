01a08b6ecd8796410c1e44bf93a305f11dcd0df9
lilliputten <lilliputten@gmail.com>
2026-09-22 19:19:23 +0300

    Issue #1: mindstack#85: Fixed an issue on locale resetting to default.

--- src/app/[locale]/RootLayout.tsx
+++ src/app/[locale]/RootLayout.tsx
@@ -86,11 +86,11 @@ export async function RootLayout(props: TRootLayoutProps) {
     // );
     // debugger; // eslint-disable-line no-debugger
     // TODO? -- Redirect to 'notFound' page?
-    // Just use the default value
-    locale = defaultLocale;
     // NOTE: ProberHunter: Invalid locale requested
     const __idMsg = '[RootLayout:ProberHunter] Suspicious locale requested';
     logJsonData(__idMsg, { locale }); // NOTE: It's the async function, but not awaiting nor catching intentionally!
+    // Just use the default value
+    locale = defaultLocale;
   }
 
   // const cookieStore = await cookies();
